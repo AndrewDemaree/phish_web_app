39d20f92cf6d67e45836e2fefe099bf0fcee99ef
briannak7 <112961902+briannak7@users.noreply.github.com>
2022-11-14 15:49:14 -0700

    adding submit.html and drop down option

--- django_project/settings.py
+++ django_project/settings.py
@@ -125,7 +125,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+MEDIA_ROOT = 'media/'
 
+#path where media is stored
+#https://djangocentral.com/uploading-images-with-django/
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
