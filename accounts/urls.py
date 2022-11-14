21bebce309c604f58b8907c0e196875a730c220e
briannak7 <112961902+briannak7@users.noreply.github.com>
2022-11-14 16:16:27 -0700

    adding submit.html and drop down option

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import SignUpView
+from .views import SignUpView, SubmitView
 
 urlpatterns = [
     path('signup/', SignUpView.as_view(), name='signup'),
+    path('submit/', SubmitView.as_view(), name='submit'),
     ]
--- music/credentials.py
+++ music/credentials.py
@@ -1,7 +1,7 @@
 from environs import Env
 
 env = Env()
-env.read_end()
+env.read_env()
 
 CLIENT_ID = env(CLIENT_ID)
 CLIENT_SECRET = env(CLIENT_SECRET)
