f76be5188af71c4dbec4bdf5e47445d98b85b443
briannak7 <112961902+briannak7@users.noreply.github.com>
2022-11-15 11:45:00 -0700

    submit.html and drop down option

--- music/credentials.py
+++ music/credentials.py
@@ -1,10 +1,11 @@
-from environs import Env
+from dotenv import load_dotenv
+import os
 
-env = Env()
-env.read_env()
 
-CLIENT_ID = env(CLIENT_ID)
-CLIENT_SECRET = env(CLIENT_SECRET)
-REDIRECT_URI = env(REDIRECT_URI)
+load_dotenv()
+
+CLIENT_ID='f2196432ca974697932f405751dbefe2'
+CLIENT_SECRET='1d6080a7ae344c16b11f28de3560fc86'
+REDIRECT_URI='http://127.0.0.1:8000/'
 
 #Will make these environment variables before pushing 
