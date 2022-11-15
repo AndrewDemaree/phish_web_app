95cf4dcec3c20767bd30b9a463d4d2afd7bfe59e
Paige <paige1.johnson@umconnect.umt.edu>
2022-11-15 11:50:53 -0700

    client hardcoding

--- music/credentials.py
+++ music/credentials.py
@@ -1,10 +1,10 @@
-from environs import Env
+from dotenv import load_dotenv
+import os
 
-env = Env()
-env.read_end()
+load_dotenv
 
-CLIENT_ID = env(CLIENT_ID)
-CLIENT_SECRET = env(CLIENT_SECRET)
-REDIRECT_URI = env(REDIRECT_URI)
+CLIENT_ID="f2196432ca974697932f405751dbefe2"
+CLIENT_SECRET="1d6080a7ae344c16b11f28de3560fc86"
+REDIRECT_URI="http://127.0.0.1:8000/"
 
 #Will make these environment variables before pushing 
